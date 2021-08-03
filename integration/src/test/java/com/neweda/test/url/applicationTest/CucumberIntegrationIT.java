7138ee3c928660d2aae670195ce55ef0c9356633
Rahul Ranjan <mail2ranjanenterprises@gmail.com>
2021-08-04 00:00:19 +0100

    Moved Integration test to separate directory

--- integration/src/test/java/com/neweda/test/url/applicationTest/CucumberIntegrationIT.java
+++ integration/src/test/java/com/neweda/test/url/applicationTest/CucumberIntegrationIT.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
  * the main class for cucumber where you configure where the features are defined and which formats of reports needed to be generated
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/features" }, plugin = { "pretty", "html:target/reports/cucumber/html",
+@CucumberOptions(features = { "integration/src/test/resources/features/" }, plugin = { "pretty", "html:target/reports/cucumber/html",
                 "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml" })
 public class CucumberIntegrationIT {
 }
